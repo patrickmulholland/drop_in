921c1a42d8b6f6999c94bd443cfc9e9ede9ba3a4
Patrick Mulholland <patrick.mulholland@gmx.de>
2013-02-19 11:56:58 +0100

    heroku5

--- db/migrate/20130121083750_create_events.rb
+++ db/migrate/20130121083750_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :user_id
-      t.text :short_description
+      t.text :short_description, :limit => nil
       t.date :date
       t.time :time
       t.string :venue
