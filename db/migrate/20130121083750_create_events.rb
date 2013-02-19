024e22857cc6105cc25aa72cad19f51023f5fc04
Patrick Mulholland <patrick.mulholland@gmx.de>
2013-02-19 12:12:37 +0100

    8

--- db/migrate/20130121083750_create_events.rb
+++ db/migrate/20130121083750_create_events.rb
@@ -2,12 +2,13 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :user_id
+      t.text :short_description, :limit => nil
       t.date :date
       t.time :time
       t.string :venue
       t.string :city
       t.string :street
-      t.text :details
+      t.text :details, :limit => nil
 
       t.timestamps
     end
