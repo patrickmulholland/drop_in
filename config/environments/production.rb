25d5e04e8476842033792ea054742eb920d84929
Patrick Mulholland <patrick.mulholland@gmx.de>
2013-02-19 12:55:20 +0100

    9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Twitter::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  #mailer:
+  config.action_mailer.default_url_options = { :host => 'fathomless-garden-5984.herokuapp.com' }
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
