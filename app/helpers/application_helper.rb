c02f4af4434087bd247e2be235fa8ee710635af4
Patrick Mulholland <patrick.mulholland@gmx.de>
2013-02-27 18:31:30 +0100

    small changes for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     Reply.where(:user_id => current_user, :commit => true).joins(:event).where("date = ?", Date.today)
   end
 
+  def count_past_commits_user
+    Reply.where(:user_id => @user, :commit => true).joins(:event).where("date < ?", Date.today).count
+  end
+  
 end
