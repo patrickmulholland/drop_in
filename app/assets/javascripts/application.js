528495b2c70c53db24771b0aed79c2a6932b223f
Patrick Mulholland <patrick.mulholland@gmx.de>
2013-02-27 16:05:08 +0100

    Added Tooltips to most important Places

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,37 +20,158 @@ $(function (){
 	$('#event_date').datepicker({ dateFormat: 'yy-mm-dd' });
 });
 
-this.tooltip = function(){	
-	/* CONFIG */		
-		xOffset = 10;
-		yOffset = 20;		
-		// these 2 variable determine popup's distance from the cursor
-		// you might want to adjust to get the right result		
-	/* END CONFIG */		
-	$("a.tooltip").hover(function(e){											  
-		this.t = this.title;
-		this.title = "";									  
-		$("body").append("<p id='tooltip'>"+ this.t +"</p>");
-		$("#tooltip")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px")
-			.delay(100)
-			.slideDown(100);		
-    },
-	function(){
-		this.title = this.t;		
-		$("#tooltip").remove();
-    });	
-	$("a.tooltip").mousemove(function(e){
-		$("#tooltip")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px");
-	});			
-};
+
+$( document ).ready( function()
+{
+    var targets = $( '[rel~=tooltip]' ),
+        target  = false,
+        tooltip = false,
+        title   = false;
+ 
+    targets.bind( 'mouseenter', function()
+    {
+        target  = $( this );
+        tip     = target.attr( 'title' );
+        tooltip = $( '<div id="tooltip"></div>' );
+ 
+        if( !tip || tip == '' )
+            return false;
+ 
+        target.removeAttr( 'title' );
+        tooltip.css( 'opacity', 0 )
+               .html( tip )
+               .appendTo( 'body' );
+ 
+        var init_tooltip = function()
+        {
+            if( $( window ).width() < tooltip.outerWidth() * 1.5 )
+                tooltip.css( 'max-width', $( window ).width() / 2 );
+            else
+                tooltip.css( 'max-width', 340 );
+ 
+            var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
+                pos_top  = target.offset().top - tooltip.outerHeight() + 55;
+ 
+            if( pos_left < 0 )
+            {
+                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
+                tooltip.addClass( 'left' );
+            }
+            else
+                tooltip.removeClass( 'left' );
+ 
+            if( pos_left + tooltip.outerWidth() > $( window ).width() )
+            {
+                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
+                tooltip.addClass( 'right' );
+            }
+            else
+                tooltip.removeClass( 'right' );
+ 
+            if( pos_top < 0 )
+            {
+                var pos_top  = target.offset().top + target.outerHeight();
+                tooltip.addClass( 'top' );
+            }
+            else
+                tooltip.removeClass( 'top' );
+ 
+            tooltip.css( { left: pos_left, top: pos_top } )
+                   .animate( { top: '+=10', opacity: 1 }, 50 );
+        };
+ 
+        init_tooltip();
+        $( window ).resize( init_tooltip );
+ 
+        var remove_tooltip = function()
+        {
+            tooltip.animate( { top: '-=10', opacity: 0 }, 50, function()
+            {
+                $( this ).remove();
+            });
+ 
+            target.attr( 'title', tip );
+        };
+ 
+        target.bind( 'mouseleave', remove_tooltip );
+        tooltip.bind( 'click', remove_tooltip );
+    });
+});
 
 
-
-// starting the script on page load
-$(document).ready(function(){
-	tooltip();
+$( document ).ready( function()
+{
+    var targets = $( '[rel~=tooltip2]' ),
+        target  = false,
+        tooltip2 = false,
+        title   = false;
+ 
+    targets.bind( 'mouseenter', function()
+    {
+        target  = $( this );
+        tip     = target.attr( 'title' );
+        tooltip2 = $( '<div id="tooltip2"></div>' );
+ 
+        if( !tip || tip == '' )
+            return false;
+ 
+        target.removeAttr( 'title' );
+        tooltip2.css( 'opacity', 0 )
+               .html( tip )
+               .appendTo( 'body' );
+ 
+        var init_tooltip2 = function()
+        {
+            if( $( window ).width() < tooltip2.outerWidth() * 1.5 )
+                tooltip2.css( 'max-width', $( window ).width() / 2 );
+            else
+                tooltip2.css( 'max-width', 340 );
+ 
+            var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip2.outerWidth() / 2 ),
+                pos_top  = target.offset().top - tooltip2.outerHeight() + 65;
+ 
+            if( pos_left < 0 )
+            {
+                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
+                tooltip2.addClass( 'left' );
+            }
+            else
+                tooltip2.removeClass( 'left' );
+ 
+            if( pos_left + tooltip2.outerWidth() > $( window ).width() )
+            {
+                pos_left = target.offset().left - tooltip2.outerWidth() + target.outerWidth() / 2 + 20;
+                tooltip2.addClass( 'right' );
+            }
+            else
+                tooltip2.removeClass( 'right' );
+ 
+            if( pos_top < 0 )
+            {
+                var pos_top  = target.offset().top + target.outerHeight();
+                tooltip2.addClass( 'top' );
+            }
+            else
+                tooltip2.removeClass( 'top' );
+ 
+            tooltip2.css( { left: pos_left, top: pos_top } )
+                   .animate( { top: '+=10', opacity: 1 }, 50 );
+        };
+ 
+        init_tooltip2();
+        $( window ).resize( init_tooltip2 );
+ 
+        var remove_tooltip2 = function()
+        {
+            tooltip2.animate( { top: '-=10', opacity: 0 }, 50, function()
+            {
+                $( this ).remove();
+            });
+ 
+            target.attr( 'title', tip );
+        };
+ 
+        target.bind( 'mouseleave', remove_tooltip2 );
+        tooltip2.bind( 'click', remove_tooltip2 );
+    });
 });
